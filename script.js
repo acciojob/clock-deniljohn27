f444fdd5a087788993cef770b0eae34371f13f34
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-21 23:16:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function time(){
+let date=new Date();
+document.getElementById("timer").innerText=
+	`${date.getMonth()}/${date.getDate()}/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} `;
+	setTimeout(time,1000)
+}
+time();
